a10ba6db644a4efcf439c48357ce820516ee6c75
Fábio Gonçalves <fabio.mt.goncalves@gmail.com>
2024-04-17 15:54:03 +0100

    ng build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-X7HNR2KJ.mjs";import"./chunk-Q6APJVML.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-I4HRYKA3.mjs";import"./chunk-62FZ2GG4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
