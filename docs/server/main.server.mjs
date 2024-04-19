076661d9646e972fd96dbfaa3d345f1a2a4f6d68
Fábio Gonçalves <fabio.mt.goncalves@gmail.com>
2024-04-19 11:31:20 +0100

    cmd project url fix, portfolio_preview img

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TLC4FTS4.mjs";import"./chunk-PHLQPA4H.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YRY53GTR.mjs";import"./chunk-PHLQPA4H.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
